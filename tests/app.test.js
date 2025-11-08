4936841a441584c37ce1f6ae66cbe976f1614bd3
Samuel Salcedo <samuelsalcedoramirez@gmail.com>
2025-11-07 19:03:28 -0500

    Fix Jest ES Modules config

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,7 +1,7 @@
 import request from "supertest";
 import app from "../app.js";
 
-test("GET / returns 200", async () => {
-  const response = await request(app).get("/");
-  expect(response.statusCode).toBe(200);
+test("GET / must respond 200", async () => {
+  const res = await request(app).get("/");
+  expect(res.statusCode).toBe(200);
 });
